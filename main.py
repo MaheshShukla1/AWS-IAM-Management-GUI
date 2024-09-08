38ab6fa21e7342b75ca69887df5ac17bbf52273f
Mahesh Shukla - Aka JailBreaker 🚀 <shuklamahesh602@gmail.com>
2024-09-08 11:58:14 +0530

    Updated main.py and implemented best practices.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import tkinter as tk
+import sys
+from PyQt5.QtWidgets import QApplication
 from iam_manager import IAMManagerApp
 
 def main():
-    root = tk.Tk()
-    app = IAMManagerApp(root)
-    root.mainloop()
+    # Create a QApplication instance (PyQt5 application object)
+    app = QApplication(sys.argv)
+    
+    # Create the main window instance of IAMManagerApp
+    window = IAMManagerApp()
+    
+    # Show the main window
+    window.show()
+    
+    # Execute the application's event loop
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
